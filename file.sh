538508facf3bc96433e0174a62ede9c1454bef36
Rajeshkanimata <rajeshkanimata123@gmail.com>
2025-12-01 16:21:16 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -12,11 +12,19 @@ sudo yum install java-17-amazon-corretto.x86_64
 
 
 #------------jenkins install-------------
+sudo yum install -y java-17-openjdk
+
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum install jenkins -y
+
+sudo yum install -y jenkins
+
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
 
 
 # ------------------install terraform ------------------
